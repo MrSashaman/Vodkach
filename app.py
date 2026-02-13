504f352d4873ab75640a005a0ae4540a9ae74925
mrSashaman_ <psasha2811@gmail.com>
2026-02-13 18:24:32 +1200

    е7

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from models import PrivateMessage
 import os
 from werkzeug.utils import secure_filename
 from models import Block
+from flask_socketio import SocketIO, emit, join_room
+from models import db, User, Message, PrivateMessage, Block, Badge
+
 
 
 app = Flask(__name__)
@@ -16,6 +19,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'supersecretkey'
 app.config['AVATAR_FOLDER'] = os.path.join('static', 'avatars')
+app.config['ADMIN_PANEL_PASSWORD'] = "superadmin123"
 
 db.init_app(app)
 
@@ -26,6 +30,17 @@ login_manager.login_view = 'login'
 with app.app_context():
     db.create_all()
 os.makedirs(app.config['AVATAR_FOLDER'], exist_ok=True)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+@socketio.on('join_private')
+def handle_join_private(data):
+    room = data['room']
+    join_room(room)
+
+@socketio.on('join_global')
+def handle_join_global():
+    join_room("global_chat")
 
 
 @login_manager.user_loader
@@ -39,6 +54,204 @@ def update_bio():
     current_user.bio = bio
     db.session.commit()
     return redirect(url_for('profile', username=current_user.username))
+
+
+@app.route('/admin/create_badge', methods=['POST'])
+@login_required
+def create_badge():
+    if not current_user.is_admin:
+        return redirect(url_for('chat'))
+
+    name = request.form.get("name")
+    color = request.form.get("color")
+
+    if name:
+        badge = Badge(name=name, color=color)
+        db.session.add(badge)
+        db.session.commit()
+
+    return redirect(url_for('admin_panel'))
+
+
+@app.route('/admin/give_badge/<int:user_id>/<int:badge_id>')
+@login_required
+def give_badge(user_id, badge_id):
+    if not current_user.is_admin:
+        return redirect(url_for('chat'))
+
+    user = User.query.get_or_404(user_id)
+    badge = Badge.query.get_or_404(badge_id)
+
+    if badge not in user.badges:
+        user.badges.append(badge)
+        db.session.commit()
+
+    return redirect(url_for('admin_panel'))
+
+
+@app.route('/admin/remove_badge/<int:user_id>/<int:badge_id>')
+@login_required
+def remove_badge(user_id, badge_id):
+    if not current_user.is_admin:
+        return redirect(url_for('chat'))
+
+    user = User.query.get_or_404(user_id)
+    badge = Badge.query.get_or_404(badge_id)
+
+    if badge in user.badges:
+        user.badges.remove(badge)
+        db.session.commit()
+
+    return redirect(url_for('admin_panel'))
+
+
+@app.route('/shop')
+@login_required
+def shop():
+    return render_template(
+        "shop.html",
+        dialog_users=get_dialog_users()
+    )
+
+
+@app.route('/get_private_messages/<username>')
+@login_required
+def get_private_messages(username):
+    other_user = User.query.filter_by(username=username).first_or_404()
+
+    messages = PrivateMessage.query.filter(
+        ((PrivateMessage.sender_id == current_user.id) &
+         (PrivateMessage.receiver_id == other_user.id)) |
+        ((PrivateMessage.sender_id == other_user.id) &
+         (PrivateMessage.receiver_id == current_user.id))
+    ).order_by(PrivateMessage.timestamp.asc()).all()
+
+    data = []
+    for msg in messages:
+        avatar = msg.sender.avatar if msg.sender.avatar and msg.sender.avatar.strip() != "" else "default_avatar.png"
+
+        data.append({
+            "username": msg.sender.username,
+            "text": msg.text,
+            "time": msg.timestamp.strftime("%H:%M"),
+            "avatar_url": url_for('static', filename='avatars/' + avatar),
+            "is_verified": msg.sender.is_verified
+        })
+
+    return jsonify(data)
+
+
+
+@app.before_request
+def check_if_banned():
+    if current_user.is_authenticated and current_user.is_banned:
+        if request.endpoint not in ('logout', 'blocked'):
+            return redirect(url_for('blocked'))
+
+
+@app.route('/blocked')
+@login_required
+def blocked():
+    return render_template('blocked.html')
+
+@app.route('/admin/ban/<int:user_id>', methods=['POST'])
+@login_required
+def ban_user(user_id):
+    if not current_user.is_admin:
+        return redirect(url_for('chat'))
+
+    user = User.query.get_or_404(user_id)
+
+    user.is_banned = True
+    user.ban_reason = request.form.get("reason")
+
+    db.session.commit()
+    return redirect(url_for('admin_panel'))
+
+
+@app.route('/admin/unban/<int:user_id>')
+@login_required
+def unban_user(user_id):
+    if not current_user.is_admin:
+        return redirect(url_for('chat'))
+
+    user = User.query.get_or_404(user_id)
+    user.is_banned = False
+    user.ban_reason = None
+    db.session.commit()
+
+    return redirect(url_for('admin_panel'))
+
+
+
+@app.route('/admin/set_rank/<int:user_id>/<rank>')
+@login_required
+def set_rank(user_id, rank):
+    if not current_user.is_admin:
+        return redirect(url_for('chat'))
+
+    user = User.query.get_or_404(user_id)
+
+    if rank == "none":
+        user.rank = None
+    else:
+        user.rank = rank
+
+    db.session.commit()
+    return redirect(url_for('admin_panel'))
+
+
+@app.route('/admin', methods=['GET', 'POST'])
+@login_required
+def admin_panel():
+
+    if not current_user.is_admin:
+        password = request.form.get("admin_password")
+        if request.method == "POST":
+            if password == app.config['ADMIN_PANEL_PASSWORD']:
+                current_user.is_admin = True
+                db.session.commit()
+            else:
+                flash("Неверный пароль администратора")
+                return redirect(url_for('admin_panel'))
+
+        if not current_user.is_admin:
+            return render_template("admin_login.html")
+
+    users = User.query.all()
+    badges = Badge.query.all()
+
+    return render_template("admin_panel.html",
+                           users=users,
+                           badges=badges)
+
+
+
+
+@app.route('/admin/toggle_admin/<int:user_id>')
+@login_required
+def toggle_admin(user_id):
+    if not current_user.is_admin:
+        return redirect(url_for('chat'))
+
+    user = User.query.get_or_404(user_id)
+    user.is_admin = not user.is_admin
+    db.session.commit()
+
+    return redirect(url_for('admin_panel'))
+
+
+@app.route('/admin/toggle_verified/<int:user_id>')
+@login_required
+def toggle_verified(user_id):
+    if not current_user.is_admin:
+        return redirect(url_for('chat'))
+
+    user = User.query.get_or_404(user_id)
+    user.is_verified = not user.is_verified
+    db.session.commit()
+
+    return redirect(url_for('admin_panel'))
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -70,6 +283,21 @@ def register():
 
     return render_template('register.html')
 
+
+
+
+@app.route('/update_extra', methods=['POST'])
+@login_required
+def update_extra():
+    current_user.marital_status = request.form.get("marital_status")
+    current_user.gender = request.form.get("gender")
+    current_user.address = request.form.get("address")
+    current_user.telegram = request.form.get("telegram")
+    current_user.discord = request.form.get("discord")
+    current_user.email = request.form.get("email")
+
+    db.session.commit()
+    return redirect(url_for('profile', username=current_user.username))
 
 
 @app.route('/upload_avatar', methods=['POST'])
@@ -148,7 +376,9 @@ def get_messages():
             "username": msg.user.username,
             "text": msg.text,
             "time": msg.timestamp.strftime("%H:%M"),
-            "avatar_url": url_for('static', filename='avatars/' + avatar)
+            "avatar_url": url_for('static', filename='avatars/' + avatar),
+            "is_verified": msg.user.is_verified,
+            "rank": msg.user.rank
         })
 
     return jsonify(data)
@@ -158,10 +388,11 @@ def get_messages():
 @app.route('/chat', methods=['GET', 'POST'])
 @login_required
 def chat():
+
     if request.method == 'POST':
         text = request.form.get('message')
 
-        if text:
+        if text and text.strip() != "":
             new_message = Message(
                 text=text,
                 user_id=current_user.id
@@ -169,20 +400,82 @@ def chat():
             db.session.add(new_message)
             db.session.commit()
 
-        return redirect(url_for('chat'))
+            avatar = current_user.avatar if current_user.avatar else "default_avatar.png"
+
+            socketio.emit('receive_global_message', {
+                "id": new_message.id,  # ВАЖНО
+                "username": current_user.username,
+                "text": text,
+                "time": new_message.timestamp.strftime("%H:%M"),
+                "avatar_url": url_for('static', filename='avatars/' + avatar),
+                "is_verified": current_user.is_verified,
+                "is_pinned": new_message.is_pinned,
+                "rank": current_user.rank
+
+            }, room="global_chat")
+
+        return "", 204
 
     messages = Message.query.order_by(Message.timestamp.asc()).all()
+
     return render_template(
-        'chat.html',
+        "chat.html",
+        messages=messages,
         dialog_users=get_dialog_users()
     )
+
+
+
+@app.route('/delete_message/<int:message_id>', methods=['POST'])
+@login_required
+def delete_message(message_id):
+    message = Message.query.get_or_404(message_id)
+
+    if message.user_id != current_user.id and not current_user.is_admin:
+        return "", 403
+
+    db.session.delete(message)
+    db.session.commit()
+
+    socketio.emit('message_deleted', {"id": message_id}, room="global_chat")
+
+    return "", 204
+
+
+
+
+@app.route('/pin_message/<int:message_id>', methods=['POST'])
+@login_required
+def pin_message(message_id):
+    message = Message.query.get_or_404(message_id)
+
+    if not current_user.is_admin:
+        return "", 403
+
+    message.is_pinned = not message.is_pinned
+    db.session.commit()
+
+    socketio.emit('message_pinned', {
+        "id": message_id,
+        "pinned": message.is_pinned
+    }, room="global_chat")
+
+    return "", 204
 
 
 @app.route('/profile/<username>')
 @login_required
 def profile(username):
     user = User.query.filter_by(username=username).first_or_404()
+
+    if user.is_banned:
+        return "Упс, похоже данный пользователь был заблокирован на нашем сайте :/"
+
     return render_template('profile.html', user=user)
+
+
+
+
 
 
 
@@ -198,10 +491,12 @@ def is_blocked(user1_id, user2_id):
 def dialog(username):
     other_user = User.query.filter_by(username=username).first_or_404()
 
-    if request.method == 'POST':
+    blocked = is_blocked(current_user.id, other_user.id)
+
+    if request.method == 'POST' and not blocked:
         text = request.form.get('message')
 
-        if text:
+        if text and text.strip() != "":
             new_message = PrivateMessage(
                 text=text,
                 sender_id=current_user.id,
@@ -210,30 +505,29 @@ def dialog(username):
             db.session.add(new_message)
             db.session.commit()
 
-        return redirect(url_for('dialog', username=username))
+            # ---- REALTIME ----
+            room = f"private_{min(current_user.id, other_user.id)}_{max(current_user.id, other_user.id)}"
 
+            avatar = current_user.avatar if current_user.avatar else "default_avatar.png"
+
+            socketio.emit('receive_private_message', {
+                "username": current_user.username,
+                "text": text,
+                "time": new_message.timestamp.strftime("%H:%M"),
+                "avatar_url": url_for('static', filename='avatars/' + avatar),
+                "is_verified": current_user.is_verified
+            }, room=room)
+
+        # Не делаем redirect — WebSocket сам добавит сообщение
+        return "", 204
+
+    # GET запрос
     messages = PrivateMessage.query.filter(
         ((PrivateMessage.sender_id == current_user.id) &
          (PrivateMessage.receiver_id == other_user.id)) |
         ((PrivateMessage.sender_id == other_user.id) &
          (PrivateMessage.receiver_id == current_user.id))
     ).order_by(PrivateMessage.timestamp.asc()).all()
-
-    blocked = is_blocked(current_user.id, other_user.id)
-
-    if request.method == 'POST' and not blocked:
-        text = request.form.get('message')
-
-        if text:
-            new_message = PrivateMessage(
-                text=text,
-                sender_id=current_user.id,
-                receiver_id=other_user.id
-            )
-            db.session.add(new_message)
-            db.session.commit()
-
-        return redirect(url_for('dialog', username=username))
 
     return render_template(
         "dialog.html",
@@ -277,4 +571,5 @@ def dialogs():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
+
--- models.py
+++ models.py
@@ -5,12 +5,37 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
+user_badges = db.Table(
+    'user_badges',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('badge_id', db.Integer, db.ForeignKey('badge.id'))
+)
+
+class Badge(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    color = db.Column(db.String(20), default="#5865f2")
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
     avatar = db.Column(db.String(255), nullable=True)
-    bio = db.Column(db.Text, nullable=True)  # НОВОЕ ПОЛЕ
+    bio = db.Column(db.Text, nullable=True)
+    is_admin = db.Column(db.Boolean, default=False)
+    is_verified = db.Column(db.Boolean, default=False)
+    is_banned = db.Column(db.Boolean, default=False)
+    ban_reason = db.Column(db.String(255), nullable=True)
+    rank = db.Column(db.String(20), default=None)
+    badges = db.relationship("Badge", secondary=user_badges, backref="users")
+
+    # --- НОВЫЕ ПОЛЯ ---
+    marital_status = db.Column(db.String(50), nullable=True)  # Женат / Не женат
+    gender = db.Column(db.String(50), nullable=True)          # Пол
+    address = db.Column(db.String(255), nullable=True)
+    telegram = db.Column(db.String(100), nullable=True)
+    discord = db.Column(db.String(100), nullable=True)
+    email = db.Column(db.String(120), nullable=True)
 
 
 class Block(db.Model):
@@ -20,11 +45,14 @@ class Block(db.Model):
 
 
 
+
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    is_pinned = db.Column(db.Boolean, default=False)
+
 
     user = db.relationship('User', backref='messages')
 
